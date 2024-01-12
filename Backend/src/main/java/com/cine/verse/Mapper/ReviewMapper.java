31f26a5a9095468219fa4b3eee67b7cc19b58aa2
mjidelouss <mjid.elouss@gmail.com>
2024-01-12 11:39:37 +0100

    Added Mapper for Review + WatchList + MyList used MapStrcut

--- Backend/src/main/java/com/cine/verse/Mapper/ReviewMapper.java
+++ Backend/src/main/java/com/cine/verse/Mapper/ReviewMapper.java
@@ -1,7 +1,11 @@
 package com.cine.verse.Mapper;
 
+import com.cine.verse.domain.Review;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface ReviewMapper {
+    Review reviewRequestToReview(ReviewRequest reviewRequest);
+
+    ReviewResponse reviewToReviewResponse(Review review);
 }
--- Backend/src/main/java/com/cine/verse/Mapper/WatchListMapper.java
+++ Backend/src/main/java/com/cine/verse/Mapper/WatchListMapper.java
@@ -1,7 +1,11 @@
 package com.cine.verse.Mapper;
 
+import com.cine.verse.domain.WatchList;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface WatchListMapper {
+    WatchList watchListRequestToWatchList(WatchListRequest watchListRequest);
+
+    WatchListResponse watchListToWatchListResponse(WatchList watchList);
 }
