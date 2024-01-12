7e8f5f6e59ee5101c103b2340a29172e0d853bfc
mjidelouss <mjid.elouss@gmail.com>
2024-01-12 10:00:19 +0100

    Added Mapper for MovieList + Movie, Using (MapStruct)

--- Backend/src/main/java/com/cine/verse/Mapper/MovieListMapper.java
+++ Backend/src/main/java/com/cine/verse/Mapper/MovieListMapper.java
@@ -1,7 +1,11 @@
 package com.cine.verse.Mapper;
 
+import com.cine.verse.domain.MovieList;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface MovieListMapper {
+    MovieList movieListRequestToMovieList(MovieListRequest movieListRequest);
+
+    MovieResponse movieListToMovieListResponse(MovieList movieList);
 }
--- Backend/src/main/java/com/cine/verse/Mapper/MovieMapper.java
+++ Backend/src/main/java/com/cine/verse/Mapper/MovieMapper.java
@@ -1,7 +1,11 @@
 package com.cine.verse.Mapper;
 
+import com.cine.verse.domain.Movie;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface MovieMapper {
+    Movie movieRequestToMovie(MovieRequest movieRequest);
+
+    MovieResponse movieToMovieResponse(Movie movie);
 }
