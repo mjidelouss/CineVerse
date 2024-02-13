10bc2c508e2f26347e0350c6c47ce176afd22d6c
mjidelouss <mjid.elouss@gmail.com>
2024-02-13 16:15:03 +0100

    Added Refresh Token Request and Response

--- Backend/src/main/java/com/cine/verse/Dto/request/RefreshTokenRequest.java
+++ Backend/src/main/java/com/cine/verse/Dto/request/RefreshTokenRequest.java
@@ -1,4 +1,12 @@
 package com.cine.verse.Dto.request;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class RefreshTokenRequest {
+    private String refreshToken;
 }
