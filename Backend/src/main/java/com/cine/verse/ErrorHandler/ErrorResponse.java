5c77dc0263cd972a45c192cdbfd12d18066ab3e7
mjidelouss <mjid.elouss@gmail.com>
2024-02-13 14:13:08 +0100

    Added Error Token Exception and handler

---
+++ Backend/src/main/java/com/cine/verse/ErrorHandler/ErrorResponse.java
@@ -0,0 +1,20 @@
+package com.cine.verse.ErrorHandler;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ErrorResponse {
+    private int status;
+    private String error;
+    private Instant timestamp;
+    private String message;
+    private String path;
+}
