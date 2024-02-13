9b9a12ced58cb28950524d2b660f74d4124c04da
mjidelouss <mjid.elouss@gmail.com>
2024-02-13 16:20:11 +0100

    Added Register Request

---
+++ Backend/src/main/java/com/cine/verse/Dto/request/RegisterRequest.java
@@ -0,0 +1,29 @@
+package com.cine.verse.Dto.request;
+
+import com.cine.verse.enums.Role;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegisterRequest {
+    @NotBlank(message = "firstname is required")
+    private String firstname;
+    @NotBlank(message = "lastname is required")
+    private String lastname;
+    @NotBlank(message = "email is required")
+    @Email(message = "email format is not valid")
+    private String email;
+    @NotBlank(message = "password is required")
+    @StrongPassword
+    private String password;
+    @NotNull
+    private Role role;
+}
