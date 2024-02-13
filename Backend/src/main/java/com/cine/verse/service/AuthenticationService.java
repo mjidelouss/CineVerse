4ec5219f715555499769e89be489aa1bcbf9b65a
mjidelouss <mjid.elouss@gmail.com>
2024-02-13 14:20:57 +0100

    Added Authentification Service and Implementation

--- Backend/src/main/java/com/cine/verse/service/AuthenticationService.java
+++ Backend/src/main/java/com/cine/verse/service/AuthenticationService.java
@@ -1,4 +1,6 @@
 package com.cine.verse.service;
 
 public interface AuthenticationService {
+    AuthenticationResponse register(RegisterRequest request);
+    AuthenticationResponse authenticate(AuthenticationRequest request);
 }
--- Backend/src/main/java/com/cine/verse/service/impl/AuthenticationServiceImpl.java
+++ Backend/src/main/java/com/cine/verse/service/impl/AuthenticationServiceImpl.java
@@ -1,5 +1,76 @@
 package com.cine.verse.service.impl;
 
-public class AuthenticationServiceImpl {
+import com.cine.verse.domain.AppUser;
+import com.cine.verse.enums.TokenType;
+import com.cine.verse.repository.AppUserRepository;
+import com.cine.verse.service.AuthenticationService;
+import com.cine.verse.service.JwtService;
+import com.cine.verse.service.RefreshTokenService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class AuthenticationServiceImpl implements AuthenticationService {
+
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final AppUserRepository userRepository;
+    private final AuthenticationManager authenticationManager;
+    private final RefreshTokenService refreshTokenService;
+    @Override
+    public AuthenticationResponse register(RegisterRequest request) {
+        var user = AppUser.builder()
+                .firstname(request.getFirstname())
+                .lastname(request.getLastname())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(request.getRole())
+                .build();
+        user = userRepository.save(user);
+        var jwt = jwtService.generateToken(user);
+        var refreshToken = refreshTokenService.createRefreshToken(user.getId());
+
+        var roles = user.getRole().getAuthorities()
+                .stream()
+                .map(SimpleGrantedAuthority::getAuthority)
+                .toList();
+
+        return AuthenticationResponse.builder()
+                .accessToken(jwt)
+                .email(user.getEmail())
+                .id(user.getId())
+                .refreshToken(refreshToken.getToken())
+                .roles(roles)
+                .tokenType( TokenType.BEARER.name())
+                .build();
+    }
+
+    @Override
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(request.getEmail(),request.getPassword()));
+
+        var user = userRepository.findByEmail(request.getEmail()).orElseThrow(() -> new IllegalArgumentException("Invalid email or password."));
+        var roles = user.getRole().getAuthorities()
+                .stream()
+                .map(SimpleGrantedAuthority::getAuthority)
+                .toList();
+        var jwt = jwtService.generateToken(user);
+        var refreshToken = refreshTokenService.createRefreshToken(user.getId());
+        return AuthenticationResponse.builder()
+                .accessToken(jwt)
+                .roles(roles)
+                .email(user.getEmail())
+                .id(user.getId())
+                .refreshToken(refreshToken.getToken())
+                .tokenType( TokenType.BEARER.name())
+                .build();
+    }
 }
