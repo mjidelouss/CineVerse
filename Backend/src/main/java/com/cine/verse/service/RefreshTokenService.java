fcc22c46f90a7b783ee20aff426514e6562f4c6b
mjidelouss <mjid.elouss@gmail.com>
2024-02-13 13:54:05 +0100

    Added Refresh Token Service and Implementation (Backend Security)

---
+++ Backend/src/main/java/com/cine/verse/service/RefreshTokenService.java
@@ -0,0 +1,19 @@
+package com.cine.verse.service;
+
+import com.cine.verse.domain.RefreshToken;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.ResponseCookie;
+
+import java.util.Optional;
+
+public interface RefreshTokenService {
+
+    RefreshToken createRefreshToken(Long userId);
+    RefreshToken verifyExpiration(RefreshToken token);
+    Optional<RefreshToken> findByToken(String token);
+    RefreshTokenResponse generateNewToken(RefreshTokenRequest request);
+    ResponseCookie generateRefreshTokenCookie(String token);
+    String getRefreshTokenFromCookies(HttpServletRequest request);
+    void deleteByToken(String token);
+    ResponseCookie getCleanRefreshTokenCookie();
+}
---
+++ Backend/src/main/java/com/cine/verse/service/impl/RefreshTokenServiceImpl.java
@@ -0,0 +1,114 @@
+package com.cine.verse.service.impl;
+
+import com.cine.verse.domain.AppUser;
+import com.cine.verse.domain.RefreshToken;
+import com.cine.verse.repository.AppUserRepository;
+import com.cine.verse.repository.RefreshTokenRepository;
+import com.cine.verse.service.RefreshTokenService;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseCookie;
+import org.springframework.stereotype.Service;
+import org.springframework.web.util.WebUtils;
+
+import java.time.Instant;
+import java.util.Base64;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class RefreshTokenServiceImpl implements RefreshTokenService {
+
+    private final AppUserRepository userRepository;
+    private final RefreshTokenRepository refreshTokenRepository;
+    private final JwtService jwtService;
+
+    @Value("${application.security.jwt.refresh-token.expiration}")
+    private long refreshExpiration;
+    @Value("${application.security.jwt.refresh-token.cookie-name}")
+    private String refreshTokenName;
+    @Override
+    public RefreshToken createRefreshToken(Long userId) {
+        AppUser user = userRepository.findById(userId).orElseThrow(() -> new RuntimeException("User not found"));
+        RefreshToken refreshToken = RefreshToken.builder()
+                .revoked(false)
+                .user(user)
+                .token(Base64.getEncoder().encodeToString(UUID.randomUUID().toString().getBytes()))
+                .expiryDate(Instant.now().plusMillis(refreshExpiration))
+                .build();
+        return refreshTokenRepository.save(refreshToken);
+    }
+
+    @Override
+    public RefreshToken verifyExpiration(RefreshToken token) {
+        if(token == null){
+            log.error("Token is null");
+            throw new TokenException(null, "Token is null");
+        }
+        if(token.getExpiryDate().compareTo(Instant.now()) < 0 ){
+            refreshTokenRepository.delete(token);
+            throw new TokenException(token.getToken(), "Refresh token was expired. Please make a new authentication request");
+        }
+        return token;
+    }
+
+    @Override
+    public Optional<RefreshToken> findByToken(String token) {
+        return refreshTokenRepository.findByToken(token);
+    }
+
+    @Override
+    public RefreshTokenResponse generateNewToken(RefreshTokenRequest request) {
+        AppUser user = refreshTokenRepository.findByToken(request.getRefreshToken())
+                .map(this::verifyExpiration)
+                .map(RefreshToken::getUser)
+                .orElseThrow(() -> new TokenException(request.getRefreshToken(),"Refresh token does not exist"));
+
+        String token = jwtService.generateToken(user);
+        return RefreshTokenResponse.builder()
+                .accessToken(token)
+                .refreshToken(request.getRefreshToken())
+                .tokenType(TokenType.BEARER.name())
+                .build();
+    }
+
+    @Override
+    public ResponseCookie generateRefreshTokenCookie(String token) {
+        return ResponseCookie.from(refreshTokenName, token)
+                .path("/")
+                .maxAge(refreshExpiration/1000) // 15 days in seconds
+                .httpOnly(true)
+                .secure(true)
+                .sameSite("Strict")
+                .build();
+    }
+
+    @Override
+    public String getRefreshTokenFromCookies(HttpServletRequest request) {
+        Cookie cookie = WebUtils.getCookie(request, refreshTokenName);
+        if (cookie != null) {
+            return cookie.getValue();
+        } else {
+            return "";
+        }
+    }
+
+    @Override
+    public void deleteByToken(String token) {
+        refreshTokenRepository.findByToken(token).ifPresent(refreshTokenRepository::delete);
+    }
+
+    @Override
+    public ResponseCookie getCleanRefreshTokenCookie() {
+        return ResponseCookie.from(refreshTokenName, "")
+                .path("/")
+                .httpOnly(true)
+                .maxAge(0)
+                .build();
+    }
+}
