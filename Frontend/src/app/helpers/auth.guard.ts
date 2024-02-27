194259da0fc6939c7a7712ef557f1eb57852882c
mjidelouss <mjid.elouss@gmail.com>
2024-02-27 09:57:53 +0100

    Added Auth Guard in frontEnd

--- Frontend/src/app/helpers/auth.guard.ts
+++ Frontend/src/app/helpers/auth.guard.ts
@@ -1,5 +1,27 @@
-import { CanActivateFn } from '@angular/router';
+import {CanActivateFn, Router} from '@angular/router';
+import {inject} from "@angular/core";
+import {map, take} from "rxjs";
+import {AuthService} from "../service/auth.service";
 
-export const authGuard: CanActivateFn = (route, state) => {
-  return true;
+export const authGuard: CanActivateFn = (
+  route,
+  state
+) => {
+  const router = inject(Router);
+  const authService = inject(AuthService);
+
+  return authService.AuthenticatedUser$.pipe(
+    take(1), // take the first one and then unsubscribe automatically
+    map(user => {
+      // check if route is restricted by role
+      const { roles } = route.data;
+      if(user && user.role && roles.includes(user.role.name)) {
+        return true;
+      }
+      if(user) {
+        return  router.createUrlTree(['/forbidden']);
+      }
+      return  router.createUrlTree(['/login']);
+    })
+  )
 };
