8f7df8ee4241877802a1cfa89b58838afb9b77ce
mjidelouss <mjid.elouss@gmail.com>
2024-03-14 14:09:48 +0000

    Added List Component

--- Frontend/src/app/component/add-list/add-list.component.ts
+++ Frontend/src/app/component/add-list/add-list.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Movie} from "../../models/movie";
 
 @Component({
   selector: 'app-add-list',
   templateUrl: './add-list.component.html',
   styleUrls: ['./add-list.component.scss']
 })
-export class AddListComponent {
+export class AddListComponent implements OnInit{
+
+  searchResults: Movie[] = []
+  listTitle: string = ''
+  listDescription: string = ''
+  searchTerm: string = ''
+  constructor() {
+  }
+  ngOnInit(): void {
+  }
+
+  addMovieToList() {
+
+  }
+
+  searchMovies() {
+
+  }
+
+  addMovie(movie: Movie) {
+
+  }
 
 }
